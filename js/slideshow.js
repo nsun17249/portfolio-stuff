f27d79a4e2c86ff582ded8f9d35ce403156a5ad3
Nalini <nsun17249@gmail.com>
2025-03-01 03:15:48 +1300

    fixed multiple instances of timers being created when dots were used

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,24 +1,39 @@
-let slideIndex = 0;
+let slideIndex = 1;
+let counter = 0;
 showSlides(slideIndex);
+timedCount();
 
 // Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
+  counter = 0;
 }
 
-function showSlides(n) {
+// counter function
+function timedCount() {
+  if (counter == 5) { // checks if counter hits 5s
+    // go to next slide and reset counter
+    showSlides(slideIndex++);
+    counter = 0;
+  } else {
+    counter++;
+  }
+  // runs timeCount every 1s
+  setTimeout(timedCount, 1000);
+}
+
+function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
+
+  if (slideIndex > slides.length) { slideIndex = 1 }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 } 
